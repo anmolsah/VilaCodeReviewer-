65a7fdb1bb608faa7b722687d260263f9f1f1631
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-11 23:38:14 +0530

    set up server with Express and add basic routing

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+const app = require("./src/app");
+
+app.listen(9876, () => {
+  console.log("server is running on port 9876");
+});
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
-const app = express(); //it creates an server and then we store it ina a variable
+const app = express(); //it creates an server and then we store it in a variable
 
- 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+module.exports = app;
