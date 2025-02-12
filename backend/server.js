5a81d0002dd99207e39641b89093cbca210906e7
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-13 00:40:13 +0530

    set server port from environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const app = require("./src/app");
 
+const PORT = process.env.PORT || 9876;
 app.listen(9876, () => {
   console.log("server is running on port 9876");
 });
