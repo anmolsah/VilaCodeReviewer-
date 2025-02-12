61571849efc6395413771a83508a3592e860cb9f
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-13 00:41:28 +0530

    refactor server port configuration to use environment variable

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 const app = require("./src/app");
 
 const PORT = process.env.PORT || 9876;
-app.listen(9876, () => {
-  console.log("server is running on port 9876");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
